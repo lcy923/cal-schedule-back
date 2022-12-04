4662af0a79e5d4c6cd35bc6163f969ae0d89366c
lcy923 <lcy923@naver.com>
2022-12-04 16:21:27 +0900

    first setup

---
+++ src/main/java/com/study/calscheduleback/CalScheduleBackApplication.java
@@ -0,0 +1,13 @@
+package com.study.calscheduleback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalScheduleBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalScheduleBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/calscheduleback/CalScheduleBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.calscheduleback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalScheduleBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
